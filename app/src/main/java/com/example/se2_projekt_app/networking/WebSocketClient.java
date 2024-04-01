d7d436515d7fcd84825f31e9dab0806794b9264f
Fabse120 <fabseH60@gmail.com>
2024-04-01 19:44:01 +0200

    Client Logik und Frontend Struktur implementiert.

--- app/src/main/java/com/example/se2_projekt_app/networking/WebSocketClient.java
+++ app/src/main/java/com/example/se2_projekt_app/networking/WebSocketClient.java
@@ -1,64 +1,81 @@
 package com.example.se2_projekt_app.networking;
 
 import android.util.Log;
-
-import androidx.annotation.NonNull;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 
-public class WebSocketClient {
+public class WebSocketClient{
     /**
-     * Client Seite für die Kommunikation des Frontend mit dem Backend
+     * Client für die Kommunikation mit dem Backend.
      */
 
-    private String websocket_url;
-    private int port;
+    private final String websocket_url= "ws://10.0.2.2:8080/welcome-to-the-moon"; //IP vom localhost
     private WebSocket webSocket;
-    public WebSocketClient(String url, int port){
-        this.websocket_url = url;
-        this.port = port;
-    }
 
     /**
-     *
+     * Stellt eine Verbindung zum Server her.
      * @param messageHandler
      */
-    public void connectToServer(WebSocketMessageHandler messageHandler) throws IllegalAccessException {
+    public void connectToServer(WebSocketMessageHandler<String> messageHandler){
         if (messageHandler == null)
-            throw new IllegalAccessException("Ein messageHandler wird benötigt");
-
+            throw new IllegalArgumentException("Ein messageHandler wird benötigt");
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(this.websocket_url).build();
 
         webSocket = client.newWebSocket(request, new WebSocketListener() {
+            /**
+             * Logik die bei bestehender Verbindung mit dem Server ausgeführt wird.
+             * @param webSocket
+             * @param response
+             */
             @Override
-            public void onOpen(@NonNull WebSocket webSocket, @NonNull Response response) {
+            public void onOpen(WebSocket webSocket, Response response) {
                 Log.d("Network", "Verbunden");
             }
+
+            /**
+             *
+             * @param webSocket
+             * @param text
+             */
 
             @Override
             public void onMessage(WebSocket webSocket, String text){
                 messageHandler.onMessageReceived(text);
             }
 
+            /**
+             * Logik die bei fehlgeschlagenem Verbindungsaufbau mit dem Server ausgeführt wird.
+             * @param webSocket
+             * @param tw
+             * @param response
+             */
+
             @Override
             public void onFailure(WebSocket webSocket, Throwable tw, Response response){
-                Log.d("Network", "Verbindung fehlgeschlagen");
+                Log.d("Network", "Verbindung fehlgeschlagen: " + tw.getMessage() + response.message());
             }
         });
     }
 
+    /**
+     * Schickt einen String an den Server.
+     * @param msg
+     */
     public void sendMessageToServer(String msg){
         webSocket.send(msg);
     }
 
+    /**
+     * Schließt die Serververbindung.
+     * @throws Throwable
+     */
     @Override
-    public void finalize() throws Throwable{
+    protected void finalize() throws Throwable{
         try{
             webSocket.close(1000, "Closing");
         } finally{
@@ -66,6 +83,13 @@ public class WebSocketClient {
         }
     }
 
+    /**
+     * Für Testzwecke benötigt.
+     * @param first
+     * @param second
+     * @return
+     */
+    // Simple method to demonstrate unit testing and test coverage with sonarcloud
     public static String concatenateStrings(String first, String second){
         return first + second;
     }
