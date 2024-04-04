8cdc105e41cef40650046c5d360a11dd9e24e742
AngelaSternig <angela-sternig@gmx.at>
2024-04-04 19:57:41 +0200

    Made variables local to avoid redundant code

--- app/src/main/java/com/example/se2_projekt_app/screens/Multiplayer.java
+++ app/src/main/java/com/example/se2_projekt_app/screens/Multiplayer.java
@@ -4,29 +4,22 @@ import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
-import android.view.View;
 import android.widget.Button;
 import com.example.se2_projekt_app.R;
 import java.util.ArrayList;
 
 public class Multiplayer extends Activity {
 
-    private RecyclerView userListView;
-    private Button startGameButton;
-
-    private Button backButton;
-    private UserListAdapter userListAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
 
-        userListView = findViewById(R.id.lobbyUserList);
-        startGameButton = findViewById(R.id.startGameButton);
-        backButton = findViewById(R.id.backButton);
+        RecyclerView userListView = findViewById(R.id.lobbyUserList);
+        Button startGameButton = findViewById(R.id.startGameButton);
+        Button backButton = findViewById(R.id.backButton);
 
-        userListAdapter = new UserListAdapter(new ArrayList<>());
+        UserListAdapter userListAdapter = new UserListAdapter(new ArrayList<>());
         userListView.setAdapter(userListAdapter);
         userListView.setLayoutManager(new LinearLayoutManager(this));
 
