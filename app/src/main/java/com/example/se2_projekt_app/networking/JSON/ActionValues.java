968e08e9569e94867fdc2c79556d91fdfa5fe215
Fabse120 <fabseH60@gmail.com>
2024-04-06 12:59:22 +0200

    Adapted creation of JSON Object. Deleted GenericJSONTemplate.json and replaced it with GenerateJSONObject.java class. Created enmus for Action key.

---
+++ app/src/main/java/com/example/se2_projekt_app/networking/JSON/ActionValues.java
@@ -0,0 +1,11 @@
+package com.example.se2_projekt_app.networking.JSON;
+
+public enum ActionValues {
+    REGISTERUSER("registerUser"),
+    JOINLOBBY("joinLobby");
+
+    private final String value;
+    ActionValues(String action){
+        this.value = action;
+    }
+}
---
+++ app/src/main/java/com/example/se2_projekt_app/networking/JSON/GenerateJSONObject.java
@@ -0,0 +1,13 @@
+package com.example.se2_projekt_app.networking.JSON;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GenerateJSONObject {
+
+    public static JSONObject generateJSONObject() throws JSONException {
+        return new JSONObject(
+                "{\"Username\":\\null,\"Action\":\\null,\"Message\":\\null}"
+        );
+    }
+}
