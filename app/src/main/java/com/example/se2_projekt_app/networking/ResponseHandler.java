bfc4c0896089f3e19816e78df223fb3e02062f52
Fabse120 <fabseH60@gmail.com>
2024-04-19 23:44:48 +0200

    Reimplemented ResponseHandler.java

--- app/src/main/java/com/example/se2_projekt_app/networking/ResponseHandler.java
+++ app/src/main/java/com/example/se2_projekt_app/networking/ResponseHandler.java
@@ -1,11 +1,12 @@
 package com.example.se2_projekt_app.networking;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Interface to handle messages from server.
  * @param <T> Any object received from server.
  */
-public interface WebSocketMessageHandler<T> {
-    void onMessageReceived(T message) throws JSONException;
+public interface ResponseHandler<T> {
+    void onMessageReceived(JSONObject message) throws JSONException;
 }
