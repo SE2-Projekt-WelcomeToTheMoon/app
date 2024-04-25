1e3a67fd0ada0ce94b3e98c3b325b70e764d4656
thomas <t1wobak@edu.aau.at>
2024-04-25 13:24:04 +0200

    Feat: Added method in to get Element from String from server

--- app/src/main/java/com/example/se2_projekt_app/game/CardController.java
+++ app/src/main/java/com/example/se2_projekt_app/game/CardController.java
@@ -27,8 +27,32 @@ public class CardController {
         return combinations;
     }
 
-    private Element getSymbolAndTranslate(String combinationStringPart) {
-        return null;
+    /***
+     * The enum on the Serverside has german Names while the client side has english names, so this takes
+     * the String with the german name and finds the corresponding element
+     * @param element Element name in german from serverside
+     * @return corresponding element
+     */
+    private Element getSymbolAndTranslate(String element) {
+
+        switch(element) {
+            case "ROBOTER":
+                return Element.ROBOT;
+            case "WASSER":
+                return Element.WATER;
+            case "PFLANZE":
+                return Element.PLANT;
+            case "ENERGIE":
+                return Element.ENERGY;
+            case "RAUMANZUG":
+                return Element.SPACESUIT;
+            case "PLANNUNG":
+                return Element.PLANNING;
+            case "ANYTHING":
+                return Element.WILDCARD;
+            default:
+                throw new IllegalArgumentException("Not part of recognized symbols");
+        }
     }
 
     /***
