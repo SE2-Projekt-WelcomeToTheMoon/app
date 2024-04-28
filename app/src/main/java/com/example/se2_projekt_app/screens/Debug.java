1d33f853acdccc68ad16e4198f390fbd6152856e
thomas <t1wobak@edu.aau.at>
2024-04-28 19:54:05 +0200

    Fixed Merge issue

--- app/src/main/java/com/example/se2_projekt_app/screens/Debug.java
+++ app/src/main/java/com/example/se2_projekt_app/screens/Debug.java
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import android.view.View;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
