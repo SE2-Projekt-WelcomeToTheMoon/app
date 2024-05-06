dc3f8d7416f91a0a7be1b656eac38d45d712a2ca
thomas <t1wobak@edu.aau.at>
2024-05-06 16:43:16 +0200

    added drawing next Symbol and number on top of image

--- app/src/main/java/com/example/se2_projekt_app/game/CardDrawView.java
+++ app/src/main/java/com/example/se2_projekt_app/game/CardDrawView.java
@@ -10,34 +10,36 @@ import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import androidx.annotation.NonNull;
+
 import com.example.se2_projekt_app.R;
 
 public class CardDrawView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private Bitmap leafBitmap;
-    private Bitmap robotBitmap;
-    private int number;
+    private final Bitmap leafBitmap;
+    private final Bitmap robotBitmap;
+
 
     public CardDrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
         getHolder().addCallback(this);
         leafBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.leaf);
         robotBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.robot);
-        number = 0;
+
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
         draw();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
         draw();
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {}
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {}
 
     private void draw() {
         Canvas canvas = null;
@@ -53,25 +55,8 @@ public class CardDrawView extends SurfaceView implements SurfaceHolder.Callback 
 
                 // Draw the image and number three times with appropriate spacing
                 for (int i = 0; i < 3; i++) {
-                    int left = i * (imageWidth + 20);
-                    int top = 0;
-                    int right = left + imageWidth;
-                    int bottom = top + imageHeight;
-
-                    canvas.drawBitmap(leafBitmap, null, new android.graphics.Rect(left, top, right, bottom), null);
-
-                    int robotWidth = imageWidth / 4;
-                    int robotHeight = imageHeight / 4;
-                    canvas.drawBitmap(robotBitmap, null, new android.graphics.Rect(left, top, left + robotWidth, top + robotHeight), null);
-
-                    Paint paint = new Paint();
-                    paint.setTextSize(50);
-                    paint.setColor(Color.BLACK);
-                    paint.setTextAlign(Paint.Align.CENTER);
-
-                    int numberX = left + (imageWidth / 2);
-                    int numberY = top + (imageHeight / 2) + 25;
-                    canvas.drawText(String.valueOf(number), numberX, numberY, paint);
+                    int left = i * (imageWidth + 20); // Adjust spacing as needed
+                    drawCombination(canvas, left, leafBitmap, robotBitmap,i, imageWidth, imageHeight);
                 }
             }
         } finally {
@@ -79,5 +64,25 @@ public class CardDrawView extends SurfaceView implements SurfaceHolder.Callback 
                 getHolder().unlockCanvasAndPost(canvas);
             }
         }
+    }
+
+    private void drawCombination(Canvas canvas, int offsetX, Bitmap currentSymbol, Bitmap nextSymbol,int currentNumber, int symbolWidth, int symbolHeight) {
+        // Draw current Symbol
+        canvas.drawBitmap(currentSymbol, null, new android.graphics.Rect(offsetX, 0, offsetX + symbolWidth, symbolHeight), null);
+
+        // Draw nextSymbol on top of Current symbol at 1/4 the size
+        int robotWidth = symbolWidth / 4;
+        int robotHeight = symbolHeight / 4;
+        canvas.drawBitmap(nextSymbol, null, new android.graphics.Rect(offsetX, 0, offsetX + robotWidth, robotHeight), null);
+
+        // Draw number on top of current Symbol
+        Paint paint = new Paint();
+        paint.setTextSize(50);
+        paint.setColor(Color.BLACK);
+        paint.setTextAlign(Paint.Align.CENTER);
+        // Calculate position to center number on Symbol
+        int numberX = offsetX + (symbolWidth / 2);
+        int numberY = symbolHeight / 2 + 25; // Adjust vertical position as needed
+        canvas.drawText(String.valueOf(currentNumber), numberX, numberY, paint);
     }
 }
