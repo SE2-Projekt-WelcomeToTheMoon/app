230b16575f0c2456a64e27ee8cc0a33cbaad720a
Fabse120 <fabseH60@gmail.com>
2024-05-07 18:18:02 +0200

    Implemented SendMessageService.java. Added method that sends a message to the server by calling the sendMessageToServer method from the WebSocketClient.java class. Method sendMessage now returns boolean true if message has been sent, and false if message is incomplete.

---
+++ app/src/main/java/com/example/se2_projekt_app/networking/services/SendMessageService.java
@@ -0,0 +1,44 @@
+package com.example.se2_projekt_app.networking.services;
+
+import android.util.Log;
+
+import com.example.se2_projekt_app.networking.WebSocketClient;
+import com.example.se2_projekt_app.screens.Username;
+
+import org.json.JSONObject;
+
+import lombok.SneakyThrows;
+
+public class SendMessageService {
+    private static final String TAG = "SendMessageService";
+    private static final WebSocketClient webSocket = Username.webSocketClient;
+
+    /**
+     * Method to send one message to server.
+     * @param messageToSend Message to send to server.
+     */
+    @SneakyThrows
+    public static boolean sendMessage(JSONObject messageToSend){
+        if(checkMessage(messageToSend)) {
+            webSocket.sendMessageToServer(messageToSend.toString());
+            Log.i(TAG, "Message sent.");
+            return true;
+        }else {
+            Log.w(TAG, "Message incomplete. Message not sent.");
+            return false;
+        }
+    }
+
+    /**
+     * Checks if must have keys are in the message.
+     * @param messageToCheck Message to check.
+     * @return Boolean value if message to send has needed keys or not.
+     */
+    @SneakyThrows
+    private static boolean checkMessage(JSONObject messageToCheck){
+        return (((messageToCheck.getString("action") != null) &&
+                !(messageToCheck.getString("action").isEmpty())) &&
+                ((messageToCheck.getString("username") != null)) &&
+                !(messageToCheck.getString("username").isEmpty()));
+    }
+}
