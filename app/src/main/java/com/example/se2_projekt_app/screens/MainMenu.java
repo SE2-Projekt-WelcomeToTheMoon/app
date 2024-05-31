a76e918bbed50cf14b1b5767b10685b42db38c50
Fabse120 <fabseH60@gmail.com>
2024-05-31 21:03:38 +0200

    Extracted exit button logic from exit methode to button declaration.

--- app/src/main/java/com/example/se2_projekt_app/screens/MainMenu.java
+++ app/src/main/java/com/example/se2_projekt_app/screens/MainMenu.java
@@ -7,14 +7,13 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.se2_projekt_app.R;
+import com.example.se2_projekt_app.networking.WebSocketClient;
 import com.example.se2_projekt_app.networking.json.JSONKeys;
 import com.example.se2_projekt_app.networking.json.JSONService;
 import com.example.se2_projekt_app.networking.responsehandler.ResponseReceiver;
 import com.example.se2_projekt_app.networking.services.SendMessageService;
 
 import org.json.JSONObject;
-
-import lombok.SneakyThrows;
 
 public class MainMenu extends Activity{
     public static ResponseReceiver responseReceiver;
@@ -29,7 +28,25 @@ public class MainMenu extends Activity{
         findViewById(R.id.startMP).setOnClickListener(this::startMP);
         findViewById(R.id.settings).setOnClickListener(this::openSettings);
         findViewById(R.id.highscore).setOnClickListener(this::openHighscore);
-        findViewById(R.id.exit).setOnClickListener(this::exit);
+        findViewById(R.id.exit).setOnClickListener(v -> {
+            // Exit the game
+            // Closing connection to server
+            WebSocketClient.setDisconPurpously(true);
+            JSONObject sendMessage = JSONService.generateJSONObject(JSONKeys.DISCONNECT.getValue(),
+                    Username.user.getUsername(),true, "", "");
+            SendMessageService.sendMessage(sendMessage);
+
+            responseReceiver = exitResponse -> {
+                boolean success = exitResponse.getBoolean(JSONKeys.SUCCESS.getValue());
+                if(success){
+                    Username.webSocketClient.disconnectFromServer();
+                    this.finishAffinity();
+                    Log.i(TAG, "User disconnected.");
+                }else{
+                    Log.w(TAG, "Username couldn't disconnect.");
+                }
+            };
+        });
         findViewById(R.id.debug).setOnClickListener(this::openDebug);
     }
 
@@ -57,33 +74,6 @@ public class MainMenu extends Activity{
         Intent intent = new Intent(this, Highscore.class);
         startActivity(intent);
         // Open the highscore
-    }
-
-    @SneakyThrows
-    public void exit(View view) {
-        // Exit the game
-        // Closing connection to server
-        JSONObject sendMessage = JSONService.generateJSONObject(JSONKeys.DISCONNECT.getValue(),
-                Username.user.getUsername(),true, "", "");
-        SendMessageService.sendMessage(sendMessage);
-
-        responseReceiver = exitResponse -> {
-            String username = exitResponse.getString(JSONKeys.USERNAME.getValue());
-            boolean success = exitResponse.getBoolean(JSONKeys.SUCCESS.getValue());
-            String message = exitResponse.getString(JSONKeys.MESSAGE.getValue());
-            if(success){
-                Username.webSocketClient.disconnectFromServer();
-                this.finishAffinity();
-                Log.i(TAG, "User disconnected.");
-            }else{
-//                runOnUiThread(() -> {
-//                    outputText.setText(message);
-//                    outputText.setBackgroundColor(0x8003DAC5);
-//                });
-                Log.w(TAG, "Username couldn't disconnect.");
-            }
-        };
-
     }
 
     public void openDebug(View view) {
