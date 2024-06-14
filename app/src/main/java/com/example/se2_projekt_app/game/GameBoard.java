29ac17f013bc0002ac1a345822c0aa12df4cea31
kaiser.philipp95@icloud.com <130212301+kaiser1101@users.noreply.github.com>
2024-06-14 20:03:29 +0200

    getter and setter for sysError

--- app/src/main/java/com/example/se2_projekt_app/game/GameBoard.java
+++ app/src/main/java/com/example/se2_projekt_app/game/GameBoard.java
@@ -7,15 +7,21 @@ import com.example.se2_projekt_app.enums.FieldValue;
 import com.example.se2_projekt_app.networking.json.FieldUpdateMessage;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class GameBoard {
     private List<Floor> floors;
 
     @Getter
     private int rockets;
+
+    @Getter
+    @Setter
+    private int sysError;
     public GameBoard() {
         this.floors = new ArrayList<>();
         rockets = 0;
+        sysError = 0;
     }
 
     /**
@@ -44,5 +50,8 @@ public class GameBoard {
 
     public void addRockets(int amount){
         rockets += amount;
+    }
+    public void addSysError(int amount){
+        sysError += amount;
     }
 }
